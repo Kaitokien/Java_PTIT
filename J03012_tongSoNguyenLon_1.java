5da430b7b56b9d5619fedd4f6c7b87f49cd03382
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:47:11 +0700

    Tổng số nguyên lớn - 1

---
+++ J03012_tongSoNguyenLon_1.java
@@ -0,0 +1,18 @@
+package codejavaptit;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class J03012_tongSoNguyenLon_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        BigInteger a = new BigInteger(s1);
+        BigInteger b = new BigInteger(s2);
+        System.out.println(a.add(b));
+//        while(t-- > 0){
+//            
+//        }
+    }
+}
