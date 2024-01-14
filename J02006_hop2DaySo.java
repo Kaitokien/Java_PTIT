3da2c05216f4321e124c38af387e02a5ac12636a
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:25:07 +0700

    Hợp của 2 dãy số

---
+++ J02006_hop2DaySo.java
@@ -0,0 +1,24 @@
+package codejavaptit;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class J02006_hop2DaySo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        TreeSet<Integer> set1 = new TreeSet<>();
+        int[] a = new int[n]; int[] b = new int[m];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+            set1.add(a[i]);
+        }
+        for(int i = 0; i < m; i++){
+            b[i] = sc.nextInt();
+            set1.add(b[i]);
+        }
+        for(int x : set1){
+            System.out.print(x + " ");
+        }
+    }
+}
