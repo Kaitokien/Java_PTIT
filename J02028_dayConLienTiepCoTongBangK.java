5d6283b2ae666234addff70d4c71ecad911262ee
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:38:50 +0700

    Dãy con liên tiếp có tổng bằng k

---
+++ J02028_dayConLienTiepCoTongBangK.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codejavaptit;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class J02028_dayConLienTiepCoTongBangK {
+    public static boolean isExist (int n, long[] a, long k) {
+        for (int i = 0; i <= n; i++)
+            if (Arrays.binarySearch(a, i + 1, a.length, a[i] + k) >= 0) {
+                return true;
+            }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            long k = sc.nextLong();
+            long[] a = new long[n+1];
+            a[0] = 0;
+            for (int i = 1; i <= n; i++) 
+                a[i] = a[i-1] + sc.nextLong();
+            System.out.println(isExist(n, a, k) ? "YES" : "NO");
+        }
+        sc.close();
+    }
+}
