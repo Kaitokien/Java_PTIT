3671692c9993ff26ff54832cbb9d88d02e150a7f
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:35:19 +0700

    Điểm cân bằng

---
+++ J02014_diemCanBang.java
@@ -0,0 +1,32 @@
+package codejavaptit;
+
+import java.util.*;
+
+public class J02014_diemCanBang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for(int i = 0; i < n; i++){
+                a[i] = sc.nextInt();
+                if(i == 0){
+                    b[i] = a[i];
+                }
+                else{
+                    b[i] = b[i - 1] + a[i];
+                }
+            }
+            int ans = -1;
+            for(int i = 1; i < n; i++){
+                if(b[i - 1] == b[n - 1] - b[i]){
+                    ans = i + 1;
+                    break;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
