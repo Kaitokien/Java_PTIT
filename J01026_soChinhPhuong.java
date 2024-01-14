708b2bdc6d3a4ee29a9e03e1984004297d09a4d1
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:22:12 +0700

    Số chính phương

---
+++ J01026_soChinhPhuong.java
@@ -0,0 +1,17 @@
+package codejavaptit;
+
+import java.util.Scanner;
+
+public class J01026_soChinhPhuong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int can = (int)Math.sqrt(n);
+            if(can * can == n)
+                System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
