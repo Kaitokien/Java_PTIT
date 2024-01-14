3b389230aeb23a4780e94c7751576311e2a80871
Kaitokien <luutrungkien107@gmail.com>
2024-01-14 13:48:11 +0700

    Hiệu số nguyên lớn - 2

---
+++ J03015_hieuSoNguyenLon_2.java
@@ -0,0 +1,13 @@
+package codejavaptit;
+
+import java.util.*;
+import java.math.BigInteger;
+
+public class J03015_hieuSoNguyenLon_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        System.out.println(a.subtract(b));
+    }
+}
